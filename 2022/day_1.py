522f21b46b7a8ceb0e4cd57f7c1c5ed07896dac1
calebthewood <calebwood.cs@gmail.com>
2022-12-02 16:33:19 -0800

    Add Day 1, start Day 2

---
+++ 2022/day_1.py
@@ -0,0 +1,38 @@
+# Part 1 - Find the Elf Carrying the most calories
+file = open("data.txt", "r")
+sum = 0
+elves = []
+max_cal = 0
+for line in file:
+    if line[0].isdigit():
+        sum = sum + int(line.rstrip())
+    else:
+        if sum > max_cal:
+            max_cal = sum
+        elves.append(int(sum))
+        sum = 0
+
+# Part 2 - Find the Elves carrying the top 3 number of calories then sum them
+
+def find_top_three(nums):
+    first = 0
+    second = 0
+    third = 0
+
+    for num in nums:
+        if num > first:
+            third = second
+            second = first
+            first = num
+        elif num > second:
+            third = second
+            second = num
+        elif num > third:
+            third = num
+    print("first: ", first)
+    print("second: ", second)
+    print("third: ", third)
+    return first + second + third
+
+
+print("Top 3 Sum: ", find_top_three(elves))
---
+++ 2022/day_2.py
@@ -0,0 +1,16 @@
+# Part 1 -
+# Scores:
+# A X Rock      1
+# B Y Paper     2
+# C Z Scissors  3
+# Loss - 0
+# Draw - 3
+# Win - 6
+
+# What would your total score be if everything goes
+# exactly according to your strategy guide?
+
+file = open("day_2_data.txt", "r")
+
+
+
