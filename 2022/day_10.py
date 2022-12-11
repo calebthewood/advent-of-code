87307ee7aa3cc222c1a6853258d3570aed3f3d79
calebthewood <calebwood.cs@gmail.com>
2022-12-10 20:35:55 -0800

    Finish pt 2

--- 2022/day_10.py
+++ 2022/day_10.py
@@ -6,7 +6,7 @@ def parse_data(data):
     output = []
     for line in data:
         if line == "noop":
-            output.append([line])
+            output.append(line)
         else:
             [cmd, num] = line.split(" ")
             output.append(cmd)
@@ -27,9 +27,35 @@ def run_instructions(instructions):
             x += val
     return signal_sum
 
+def render_program(instructions):
+    output = []
+    cycle = 0
+    x = 1
+    row = ""
+    count = 0
+    print("inst len: ", len(instructions))
+    for val in instructions:
+        count += 1
+        if cycle == 40:
+            output.append(f"->{row}<- Cycle: {cycle} X: {x}")
+            row = ""
+            cycle = 0
+        if cycle >= x - 1 and cycle <= x + 1:
+            row += "#"
+        else:
+            row += "."
+        if isinstance(val, int):
+            x += val
+        cycle += 1
+    output.append(f"->{row}<- Cycle: {cycle} X: {x}")
+    for row in output: print(row)
+    return "Fin!"
+
+
+
 def run_program():
     input_list = file_to_list("day_10_data.txt")
     program_instructions = parse_data(input_list)
-    return run_instructions(program_instructions)
+    return render_program(program_instructions)
 
 print(run_program())
